f8ad44507e6dfa14c088e4217f204f67fee9a750
Marek Marecki <marek.marecki@ctm.gdynia.pl>
2017-12-18 07:51:41 +0100

    Rework how colorisation works
    Previously it only checked if a line .startswith(...) a prefix.
    Now there are matcher-coloriser objects which either match a line or
    not based on arbitrary logic.
    If they return a match, the line will be colorised.

--- svn-color.py
+++ svn-color.py
@@ -17,7 +17,7 @@
  Date: December 15, 2017
 """
 
-import os, sys, subprocess
+import os, sys, subprocess, re
 
 colorizedSubcommands = (
     'status',
@@ -32,21 +32,153 @@ colorizedSubcommands = (
     'up',
 )
 
-statusColors = {
-    'M'     : "31",     # red 
-    '?'    : "37",     # grey
-    'A'     : "32",     # green
-    'X'     : "33",     # yellow
-    'C'     : "30;41",  # black on red
-    '-'     : "31",     # red
-    'D'     : "31;1",   # bold red
-    '+'    : "32",     # green
-}
+
+class Matcher:
+    def match(self, line):
+        if hasattr(self, 'starts_with'):
+            return line.startswith(self.starts_with)
+        if hasattr(self, 'starts_with_regex'):
+            return re.match(self.starts_with_regex, line) is not None
+        return False
+
+    def apply_color(self, color, text):
+        return ''.join(("\033[", color, 'm', text, "\033[m"))
+
+    def colorise(self, line):
+        if hasattr(self, 'color'):
+            return self.apply_color(self.color, line)
+        return line
+
+class AtRevision(Matcher):
+    starts_with = 'At revision'
+
+    def colorise(self, line):
+        matched = re.match(re.compile('^At revision (\d+)\.$'), line)
+        revision = self.apply_color('38;5;255;1', matched.group(1))
+        return 'At revision {}.\n'.format(revision)
+
+class UpdatedToRevision(Matcher):
+    starts_with = 'Updated to revision'
+
+    def colorise(self, line):
+        matched = re.match(re.compile('^Updated to revision (\d+)\.$'), line)
+        revision = self.apply_color('38;5;255;1', matched.group(1))
+        return 'Updated to revision {}.\n'.format(revision)
+
+class CommitedRevision(Matcher):
+    starts_with = 'Commited revision'
+
+    def colorise(self, line):
+        matched = re.match(re.compile('^Commited revision (\d+)\.$'), line)
+        revision = self.apply_color('38;5;255;1', matched.group(1))
+        return 'Commited revision {}.\n'.format(revision)
+
+class StatusModified(Matcher):
+    starts_with_regex = re.compile('^M\b')
+    color = '32'
+
+class LogModified(Matcher):
+    starts_with = '   M'
+
+    def colorise(self, line):
+        return self.apply_color('38;5;34;1', line[:4]) + self.apply_color('97', line[4:])
+
+class LogIndex(Matcher):
+    starts_with = 'Index: '
+
+    def colorise(self, line):
+        return self.apply_color('37;1', line[:len(self.starts_with)]) + self.apply_color('97', line[len(self.starts_with):])
+
+class Untracked(Matcher):
+    starts_with = '?'
+    color = '37'
+
+class StatusAdded(Matcher):
+    starts_with_regex = re.compile('^A\b')
+    color = '32'
+
+class LogAdded(Matcher):
+    starts_with = '   A'
+
+    def colorise(self, line):
+        return self.apply_color('38;5;82;1', line[:4]) + self.apply_color('97', line[4:])
+
+class StatusX(Matcher):
+    starts_with_regex = re.compile('^X\b')
+    color = '31'
+
+class StatusC(Matcher):
+    starts_with_regex = re.compile('^C\b')
+    color = '30;41'
+
+class DiffRemoved(Matcher):
+    starts_with = '-'
+    color = '31'
+
+    def match(self, line):
+        return line.startswith('-') and not line.startswith('-----')
+
+class StatusD(Matcher):
+    starts_with_regex = re.compile('^D\b')
+    color = '31;1'  # bold red
+
+class StatusU(Matcher):
+    starts_with_regex = re.compile('^U\b')
+    color = '32;1'  # bold green
+
+class StatusG(Matcher):
+    starts_with_regex = re.compile('^G\b')
+    color = '32;1'  # bold green
+
+class DiffAdded(Matcher):
+    starts_with = '+'
+    color = '32'
+
+class DiffMarker(Matcher):
+    starts_with = '@@'
+    color = '38;5;75'
+
+class RevisionHeader(Matcher):
+    matching_regex = re.compile('^(r\d+) \| (.*?) \| (.*?) \| (\d+ lines?)$')
+    color = '38;5;202'
+
+    def match(self, line):
+        return re.match(self.matching_regex, line) is not None
+
+    def colorise(self, line):
+        matched = re.match(self.matching_regex, line)
+        revision = self.apply_color('38;5;255;1', matched.group(1))
+        user = self.apply_color('38;5;220', matched.group(2))
+        date_and_time = matched.group(3)
+        lines_changed = self.apply_color('38;5;46;1', matched.group(4))
+        return ' | '.join((revision, user, date_and_time, lines_changed)) + '\n'
+
+
+
+statusColors = (
+    AtRevision(),
+    UpdatedToRevision(),
+    StatusModified(),
+    LogModified(),
+    LogIndex(),
+    Untracked(),
+    StatusAdded(),
+    LogAdded(),
+    StatusX(),
+    StatusC(),
+    DiffRemoved(),
+    StatusD(),
+    StatusU(),
+    StatusG(),
+    DiffAdded(),
+    DiffMarker(),
+    RevisionHeader(),
+)
 
 def colorize(line):
-    for status in statusColors:
-        if line.startswith(status):
-            return ''.join(("\033[", statusColors[status], "m", line, "\033[m"))
+    for each in statusColors:
+        if each.match(line):
+            return each.colorise(line)
     else:
         return line
 
